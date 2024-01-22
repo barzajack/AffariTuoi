1c7093b685a182ef52c42f1d418e3b06c6a39402
barzajack <117721941+barzajack@users.noreply.github.com>
2024-01-22 23:25:52 +0100

    Sistemazioni grafice e indentazione codice
    Co-Authored-By: Axel Ferrufino <98823712+Axel0203@users.noreply.github.com>

--- script.js
+++ script.js
@@ -2,9 +2,14 @@
 
 let pacchetti = []
 let valori = [0, 1, 5, 10, 20, 50, 75, 100, 200, 500, 5000, 10000, 15000, 20000, 30000, 50000, 75000, 100000, 200000, 300000]
+let valoriPrint = [0, 1, 5, 10, 20, 50, 75, 100, 200, 500, 5000, 10000, 15000, 20000, 30000, 50000, 75000, 100000, 200000, 300000]
+
+let pacchiBlu = [0, 1, 5, 10, 20, 50, 75, 100, 200, 500]
+let pacchiRed = [5000,10000, 15000, 20000, 30000, 50000, 75000, 100000, 200000, 300000]
+
 let numeri = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
-//Assegnazione valori al numero
+//Creo le coppie numero-valore e le inserisco come oggetto in pacchetti
 numeri.forEach((number) => {
     pacchetti.push({
         numero: number,
@@ -20,25 +25,23 @@ const userPacchettoButton = document.querySelector("#n" + userNumber)
 userPacchettoButton.style.display = "none"
 document.querySelector("#userButton").innerHTML = userNumber.toString();
 
-console.log(userPacchettoButton)
+
 pacchetti.forEach((pacco) =>{
     console.log(pacco);
 })
 
-
+//Rimozione oggetto pacchetto da array pacchetti e disabilitazione bottone
 const buttons = document.querySelectorAll(".grid-button")
-
 buttons.forEach((elem) =>{
     elem.disabled = false
     elem.addEventListener("click",(e) =>{
         const id = Number(e.target.id)
         e.target.disabled = true
         pacchetti = pacchetti.filter(pacchetto => pacchetto.numero != id)
+
         console.log(pacchetti)
     })
 })
-
-
 
 function getRandomAndRemove(array) {
     // Verifica se l'array è vuoto
@@ -46,13 +49,40 @@ function getRandomAndRemove(array) {
         console.error("L'array è vuoto.");
         return null;
     }
-    // Genera un indice casuale
+// Genera un indice casuale
     const randomIndex = Math.floor(Math.random() * array.length);
-    // Prendi l'elemento corrispondente all'indice casuale
+// Prendi l'elemento corrispondente all'indice casuale
     const randomElement = array[randomIndex];
-    // Rimuovi l'elemento dall'array
+// Rimuovi l'elemento dall'array
     array.splice(randomIndex, 1);
-    // Ritorna l'elemento ottenuto
+// Ritorna l'elemento ottenuto
     return randomElement;
 }
 
+//Inserimento dei valori pacchiBlu e pacchiRossi nelle barre laterali
+const leftBar= document.querySelector(".c1")
+const rightBar = document.querySelector(".c3")
+
+for (let i=0; i<10; i++){
+    leftBar.innerHTML += `<div class="pacchiBlu">` +
+        `<h1>€ ${pacchiBlu[i]}</h1>`+
+        `</div>`
+
+    rightBar.innerHTML+= `<div class="pacchiRed">` +
+        `<h1>€ ${pacchiRed[i]}</h1>`+
+        `</div>`
+}
+
+
+function aggiornaNumeri(){
+    leftBar.innerHTML = ""
+    rightBar.innerHTML = ""
+
+    leftBar.innerHTML += `<div class="pacchiBlu">` +
+        `<h1>€ ${pacchiBlu[i]}</h1>`+
+        `</div>`
+
+    rightBar.innerHTML+= `<div class="pacchiRed">` +
+        `<h1>€ ${pacchiRed[i]}</h1>`+
+        `</div>`
+}
