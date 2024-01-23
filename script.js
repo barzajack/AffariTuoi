96475b4ced5e0fb4cf321ac670d51ce30e2d92e0
barzajack02 <giacomo.barzaghi@yahoo.com>
2024-01-23 10:53:29 +0100

    sistemazioni js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Affari Tuoi 22/01/2024, Axel e Giacomo
 
 let pacchetti = []
+
 let valori = [0, 1, 5, 10, 20, 50, 75, 100, 200, 500, 5000, 10000, 15000, 20000, 30000, 50000, 75000, 100000, 200000, 300000]
-let valoriPrint = [0, 1, 5, 10, 20, 50, 75, 100, 200, 500, 5000, 10000, 15000, 20000, 30000, 50000, 75000, 100000, 200000, 300000]
+let numeri = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
 let pacchiBlu = [0, 1, 5, 10, 20, 50, 75, 100, 200, 500]
 let pacchiRed = [5000,10000, 15000, 20000, 30000, 50000, 75000, 100000, 200000, 300000]
 
-let numeri = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
 //Creo le coppie numero-valore e le inserisco come oggetto in pacchetti
 numeri.forEach((number) => {
@@ -38,24 +38,51 @@ buttons.forEach((elem) =>{
         const id = Number(e.target.id)
         e.target.disabled = true
         pacchetti = pacchetti.filter(pacchetto => pacchetto.numero != id)
-
         console.log(pacchetti)
+
+
+        const pacchettoSelezionato = pacchetti.find((pacco) => pacco.numero === id);
+        if (pacchettoSelezionato) {
+            const valorePacchetto = pacchettoSelezionato.valore;
+            console.log(`Valore del pacchetto associato al bottone ${id}: € ${valorePacchetto}`);
+        } else {
+            console.error(`Pacchetto non trovato per il bottone ${id}`);
+        }
+
     })
 })
 
+
+//Rimozione dagli array pacchiRed e pacchiBlu
+/*
+buttons.forEach((elem) => {
+    elem.addEventListener("click", (e) => {
+        const id = Number(e.target.id);
+        const pacchettoSelezionato = pacchetti.find((pacco) => pacco.numero === id);
+
+        if (pacchettoSelezionato) {
+            const valorePacchetto = pacchettoSelezionato.valore;
+            console.log(`Valore del pacchetto associato al bottone ${id}: € ${valorePacchetto}`);
+        } else {
+            console.error(`Pacchetto non trovato per il bottone ${id}`);
+        }
+
+        e.target.disabled = true;
+        pacchetti = pacchetti.filter((pacchetto) => pacchetto.numero !== id);
+    });
+});
+
+
+ */
+
 function getRandomAndRemove(array) {
-    // Verifica se l'array è vuoto
     if (array.length === 0) {
         console.error("L'array è vuoto.");
         return null;
     }
-// Genera un indice casuale
     const randomIndex = Math.floor(Math.random() * array.length);
-// Prendi l'elemento corrispondente all'indice casuale
     const randomElement = array[randomIndex];
-// Rimuovi l'elemento dall'array
     array.splice(randomIndex, 1);
-// Ritorna l'elemento ottenuto
     return randomElement;
 }
 
@@ -63,17 +90,19 @@ function getRandomAndRemove(array) {
 const leftBar= document.querySelector(".c1")
 const rightBar = document.querySelector(".c3")
 
-for (let i=0; i<10; i++){
+for (let i=0; i<pacchiBlu.length; i++) {
     leftBar.innerHTML += `<div class="pacchiBlu">` +
-        `<h1>€ ${pacchiBlu[i]}</h1>`+
+        `<h1>€ ${pacchiBlu[i]}</h1>` +
         `</div>`
+}
 
+for(let i=0; i<pacchiRed.length; i++){
     rightBar.innerHTML+= `<div class="pacchiRed">` +
         `<h1>€ ${pacchiRed[i]}</h1>`+
         `</div>`
 }
 
-
+/*
 function aggiornaNumeri(){
     leftBar.innerHTML = ""
     rightBar.innerHTML = ""
@@ -86,3 +115,4 @@ function aggiornaNumeri(){
         `<h1>€ ${pacchiRed[i]}</h1>`+
         `</div>`
 }
+*/
